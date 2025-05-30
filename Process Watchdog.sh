f0a6a4564850916c361a7d3cb804c3c8692b75d2
RazorCrest00 <146749797+RazorCrest00@users.noreply.github.com>
2025-05-29 21:40:48 -0700

    Watchdog Update

--- Process Watchdog.sh
+++ Process Watchdog.sh
@@ -3,16 +3,19 @@
 prev_count=0
 interval=5
 
-# colors
+# Colors and formatting
 RED='\033[1;31m'
-NC='\033[0m'  # No color
+YELLOW='\033[1;33m'
+NC='\033[0m'   # No Color
+BOLD='\033[1m'
+BLINK='\033[5m'
 
 while true; do
     current_count=$(ss -plant | tail -n +2 | wc -l)
 
     if [ "$current_count" -ne "$prev_count" ]; then
-        echo -e "${RED}⚠️ CONNECTION COUNT CHANGED: $prev_count → $current_count${NC>
-
+        echo -e "${BLINK}${BOLD}${RED}⚠️ CONNECTION COUNT CHANGED: ${YELLOW}${prev_count} → ${current_count}${NC}"
+        beep
         prev_count=$current_count
     fi
 
