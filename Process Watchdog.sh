35b9f56529b43cc54bb012104d1915f4a4fdd61d
RazorCrest00 <146749797+RazorCrest00@users.noreply.github.com>
2025-05-29 22:30:16 -0700

    Watchdog Final
    Added more vibrant colors for easy visibility and added a notice counter to easily keep track of process count changes.

--- Process Watchdog.sh
+++ Process Watchdog.sh
@@ -6,17 +6,17 @@ notice_number=1
 
 # colors and formatting
 red='\033[1;31m'
-yellow='\033[1;33m'
+yellow='\033[1;93m'
+green='\033[1;92m'
 nc='\033[0m'   # no color
 bold='\033[1m'
-blink='\033[5m'
 
 
 while true; do
     current_count=$(ss -plant | tail -n +2 | wc -l)
 
     if [ "$current_count" -ne "$prev_count" ]; then
-        echo -e "${blink}${bold}${red}hey... something changed (notice #${notice_number}):${nc} ${yellow}${prev_count} → ${current_count}${nc}"
+        echo -e "${bold}${red}Process Count Change, ${green}[NOTICE_${notice_number}]:${nc} ${yellow}${prev_count} → ${current_count}${nc}"
         prev_count=$current_count
         ((notice_number++))
     fi
